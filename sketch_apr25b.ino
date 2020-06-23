55b81044fe17757f8b218b494891161ac04f356e
Moiz Kharodawala <59765430+MoizKharodawala24@users.noreply.github.com>
2020-06-23 18:03:26 +0530

    Add files via upload

---
+++ sketch_apr25b.ino
@@ -0,0 +1,27 @@
+int trigPin = 4;
+int echoPin = 2;
+void setup() {
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  Serial.begin(9600);
+}
+void loop() {
+  long duration, distance;
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration/2)*0.034;
+  if(distance >= 200 || distance <= 0)
+  { 
+    Serial.println("Out of range");
+  }
+  else
+  {
+    Serial.print(distance);
+    Serial.println(" cm");
+  }
+  delay(500);
+}
